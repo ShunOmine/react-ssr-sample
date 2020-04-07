fb4f2b05558f8b829bb9c622776a97fcb2aa8278
ShunOmine <shun12baseball@gmail.com>
2020-04-07 15:53:59 +0900

    add: babel, react導入

--- server.ts
+++ server.ts
@@ -7,5 +7,6 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (_, res) => {
-  res.send(ssr());
+  const response = ssr();
+  res.send(response);
 });
---
+++ views/ssr.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.ssr = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _server = require("react-dom/server");
+
+var _CountUp = require("./CountUp");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var ssr = function ssr() {
+  return "\n  <html>\n    <head>\n      <title>CountUp</title>\n      <meta charset=\"utf-8\"/>\n    </head>\n    <body>\n      <div id=\"app\">\n        ".concat((0, _server.renderToString)( /*#__PURE__*/_react["default"].createElement(_CountUp.CountUp, null)), "\n      </div>\n    </body>\n  </html>\n");
+};
+
+exports.ssr = ssr;
